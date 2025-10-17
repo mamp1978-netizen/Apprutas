6f044f5e9573076f2ca9a9bd2c0fd2c5f6f42393
mamp1978-netizen <mamp1978@gmail.com>
2025-10-17 13:58:48 +0000

    error

--- tab_viajero.py
+++ tab_viajero.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from utils import address_input, resolve_selection, build_gmaps_url, make_qr
+from app_utils import address_input, resolve_selection, build_gmaps_url, make_qr
 
 def mostrar_viajero():
     st.subheader("Plan rápido (viajero)")
