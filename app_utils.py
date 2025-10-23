bab5e04842e1d5cd0f8fffc2eaa8209651f9e56e
mamp1978-netizen <mamp1978@gmail.com>
2025-10-23 21:14:13 +0200

    Refactor imports from app_utils_core for clarity

--- app_utils.py
+++ app_utils.py
@@ -1,1 +1,8 @@
-from app_utils_core import *
+from app_utils_core import (
+    suggest_addresses,
+    build_gmaps_url,
+    build_waze_url,
+    build_apple_maps_url,
+    set_location_bias,
+    use_ip_bias,
+)
